ce2ac5d38ddc0ad85a17a2132df37cfa19e889a6
Crocheter <abisolaodutayo@yahoo.com>
2022-05-02 17:02:33 -0700

    first commit

---
+++ Reg-task/src/components/Reg.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="row d-flex">
+        <div class="col-5">
+            <img src="./assets/img/bg.png" alt="">
+        </div>
+        <div class="container col-7">
+            
+        </div>
+    </div>
+</template>
